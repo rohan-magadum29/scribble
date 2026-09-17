c813c662cefcf4f86c0bb30a0c76e70464119981
rohan <rohan.magadum@fyntune.com>
2026-09-18 01:18:48 +0530

    Remove Allowed Dev Origins

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  allowedDevOrigins : [process.env.CLIENT_URL!]
 };
 
 export default nextConfig;
